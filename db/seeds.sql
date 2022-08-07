0082e0991e7eec0a753a3a88d1dd97e4b97d7a62
LisaTLe <lisa.le1127@gmail.com>
2022-08-07 09:27:15 -0600

    insert values

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,18 @@ INSERT INTO departments (name)
     ('Engineer'),
     ('Legal'),
     ('Finance');
+
+INSERT INTO role ( title, salary, department_id)
+    VALUES
+    ("Sales Lead", 80000, 1),
+    ("Engineer", 95000, 2),
+    ("Lawyer", 125000, 3),
+    ("Accountant", 70000, 4);
+
+INSERT INTO employee (first_name, last_name, emp_role, emp_manager)
+    VALUES
+    ("Patrick", "Mott", 2, NULL),
+    ("Jenny", "Liu", 4, 1),
+    ("Devon", "Smith", 1, NULL),
+    ("Dustin", "Nguyen", 2, 3),
+    ("Noelle", "Sanchez", 4, NULL);
